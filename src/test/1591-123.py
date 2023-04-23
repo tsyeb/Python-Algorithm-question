74b05f683cc78182e27631d605a1b94b6ba3fd89
Yeb <131509609+tsyeb@users.noreply.github.com>
2023-04-23 23:11:50 +0800

    屁都不会，全忘了

--- src/test/1591-123.py
+++ src/test/1591-123.py
@@ -1,45 +1,76 @@
-# 前缀和  二分法定位
+def jiechen(num):
 
-def check(n):
+    ans = 0
+    while num:
+        ans += num
+        num -= 1
+    return ans
 
-    if one_ls[n] <= aim_L:
+def fanjiechen(num, d):
+
+    ans = 0
+    while d:
+        ans += num
+        num -= 1
+        d -= 1
+    return ans
+
+def checkL(mid):
+
+    if one_ls[mid] <= aim_L:
         return True
     else:
         return False
 
 
-aim_L,aim_R = map(int, input().split())
+def checkR(mid):
+
+    if one_ls[mid] <= aim_R:
+        return True
+    else:
+        return False
+
+
+aim_L, aim_R = map(int, input().split())
 one_ls = [1]
 two_ls = [1]
 i = 1
-while i < 10000000:
+while i < 1000000:
     i += 1
     one_ls.append(one_ls[-1] + i)
     two_ls.append(two_ls[-1] + one_ls[-1])
+print(one_ls[:30])
+print(two_ls[:30])
 
-# print(one_ls[:50])
-# print(two_ls[:50])
-
-left = 0
-right = int(1e7)
-
-while left < right:
-    mid = (right - (right - left)) >> 1
-    if check(mid):
-        left = mid + 1
+L_left = 0
+L_right = int(1e5)
+while L_left < L_right:
+    mid = (L_left + L_right) // 2
+    if checkL(mid):
+        L_left = mid + 1
     else:
-        right = mid
-L = left - 1
+        L_right = mid
+L = L_left - 1
 
-l = 0
-r = int(1e7)
-while left <= right:
-    mid = (l + r + 1) // 2
-    if check(mid):
-        l = mid
+R_left = 0
+R_right = int(1e5)
+while R_left < R_right:
+    mid = (R_left + R_right + 1) // 2
+    if checkR(mid):
+        R_left = mid
     else:
-        r = mid - 1
-R = l
-num = two_ls[R] - two_ls[L]
+        R_right = mid - 1
+R = R_left
 
-print(num)
+print(L)
+print(R)
+print(one_ls[L])
+print(one_ls[R + 1])
+print(two_ls[R] - two_ls[L])
+
+print('-' * 10)
+
+ansnum = two_ls[R + 1] - two_ls[L]
+ansnum -= jiechen(aim_L - one_ls[L] - 1)
+ansnum -= fanjiechen(R - 1, one_ls[R + 1] - aim_R)
+print(ansnum)
