1c392c4a7d9d446b685e287363935552e862af07
Yeb <131509609+tsyeb@users.noreply.github.com>
2023-04-26 01:22:19 +0800

    屁都不会，全忘了

--- src/test/近似gcd.py
+++ src/test/近似gcd.py
@@ -1,24 +1,13 @@
 n, g = map(int, input().split())
-ls = list(map(int, input().split()))
+line = [0] + list(map(int, input().split()))
 
-cnt = 0
-i = 0
-j = -1
 ans = 0
-
-def is_gcd(a, g) :
-
-    if a % g == 0 : return True
-    else : return False
-
-while j < n :
-    if cnt < 2 :
-        i += 1
-        ans += 1
-        if is_gcd(ls[i], g) : pass
-        else : cnt += 1
-    j += 1
-    if is_gcd(ls[j], g) : pass
-    else : cnt -= 1
+last = 0
+j = 1
+for i in range(1, n + 1) :
+    if line[i] % g != 0 :
+        j = last + 1
+        last = i
+    if i - j + 1 >= 2 : ans += i - j  # 啊啊啊啊啊啊啊啊啊啊啊啊！！！！！！！！！！！！！
 
 print(ans)
